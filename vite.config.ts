8a6a607fdcb434c7b93a3efc41b73d927a3083a9
sofyyarar2001-alt <sofyyarar2001@gmail.com>
2026-09-14 21:11:12 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
+
+    prerender: {
+      enabled: true,
+      crawlLinks: true,
+      failOnError: true,
+    },
   },
 
   vite: {
